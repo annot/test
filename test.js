935c5c66a7915c30e25714342cd5aa3d11434af4
agaric <agaric@protonmail.com>
2016-11-07 13:19:49 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,22 +1,7 @@
-// Annot.
+// intro
 
-function intro(annot.io) {
-
-  while (reading_code) {
-
-    if !understand {
-      type(annot.io)  // visit annot.io now
-      read(annotations)
-    }
-
-    else if understand {
-      type(annot.io)
-      write(annotations)
-      help(others)
-    }
-
-  }
-
+function intro() {
+    foo()
 }
 
 // EOF
